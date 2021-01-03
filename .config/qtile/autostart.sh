8f347707c383392a80e72823a34ba54eb11f5de6
Evangelos Michelioudakis <vagmcs@gmail.com>
2021-01-03 15:57:48 +0200

    xresources config

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+xrdb ${XDG_CONFIG_HOME:-$HOME/.config}/x11/xresources
 setbg &								# restore the background using the setbg script
 picom &								# composer
 dunst &								# dunst for notifications
