8f52de1fd9c6e4730e469fea826fe1e63929cc5b
Evangelos Michelioudakis <evangelos@pollfish.com>
2023-02-18 13:26:04 +0200

    adds dependencies to ipython config

--- .config/ipython/profile_default/ipython_config.py
+++ .config/ipython/profile_default/ipython_config.py
@@ -4,8 +4,11 @@ c.InteractiveShell.autoindent = True
 c.InteractiveShell.confirm_exit = False
 
 c.InteractiveShellApp.exec_lines = [
+    'import scipy',
     'import numpy as np',
     'import pandas as pd',
-    'import matplotlib.pyplot as plt'
+    'import matplotlib.pyplot as plt',
+    'import more_itertools as mit',
+    'from faker import Faker'
 ]
 
