cd41336673ca8e0016e3c113dd9b615830235fb6
Evangelos Michelioudakis <vagmcs@gmail.com>
2025-10-06 09:58:48 +0200

    cheatsheet script

--- .config/ipython/profile_default/ipython_config.py
+++ .config/ipython/profile_default/ipython_config.py
@@ -1,10 +1,15 @@
-c = get_config()
+import warnings
 
+# ignore warnings related to virtualenv
+warnings.filterwarnings("ignore", message=".*virtualenv.*")
+
+c = get_config()  # type: ignore
+
+# automatically indent code blocks and disable exit confirmation
 c.InteractiveShell.autoindent = True
 c.InteractiveShell.confirm_exit = False
 
 c.InteractiveShellApp.exec_lines = [
-    'import numpy as np',
-    'import pandas as pd',
+    "import warnings",
+    'warnings.filterwarnings("ignore", message=".*virtualenv.*")',
 ]
-
